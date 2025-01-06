879520eaf804379bc9e395461ddd7fafa6738a30
EvilCommunist <AT2415216@yandex.ru>
2025-01-06 05:19:40 +0300

    HomePage in progress...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <template>
   <main>
-    <h1>Hello there</h1>
+    <section id="advert">
+      <div class="darkness"></div>
+      <div class="container flex">
+        <h1>Лучшее снаряжение для исторических<br>европейских боевых искусств</h1>
+      </div>
+      <img src="../components/Assets/bannerSword.png">
+    </section>
   </main>
 </template>
+
+<style scoped lang="less">
+@import "./Styles/homeStyle.less";
+</style>
