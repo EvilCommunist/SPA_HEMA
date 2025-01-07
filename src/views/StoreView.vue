f102f86feda0478d52870db4d908ea34496d3ae6
EvilCommunist <AT2415216@yandex.ru>
2025-01-07 18:39:03 +0300

    Starting making catalog

--- src/views/StoreView.vue
+++ src/views/StoreView.vue
@@ -1,5 +1,51 @@
 <template>
     <main>
-      <h1>Hello store!</h1>
+      <section id="store">
+        <div class="vertical-text top decor">Лево</div>
+        <div class="vertical-text bottom decor">Право</div>
+        <div class="container flex">
+          <aside id="menu">
+            <span id="nameSection">Каталог</span>
+            <ul>
+              <li>
+                <span id="sub_section">Защита</span>
+                <ul>
+                  <li>Защита головы</li>
+                  <li>Защита тела</li>
+                  <li>Защита рук</li>
+                  <li>Защита ног</li>
+                </ul>
+              </li>
+              <li>
+                <span id="sub_section">Оружие</span>
+                <ul>
+                  <li>Древковое</li>
+                  <li>Колющее</li>
+                  <li>Режущее оружие</li>
+                  <li>Луки</li>
+                  <li>Тренировочное</li>
+                </ul>
+              </li>
+              <li>
+                <span id="sub_section">Комплекты</span>
+                <ul>
+                  <li>Русские комплекты</li>
+                  <li>Японские комплекты</li>
+                  <li>Азиатские комплекты</li>
+                  <li>Европейские комплекты</li>
+                  <li>Арабские комплекты</li>
+                </ul>
+              </li>
+            </ul>
+          </aside>
+          <section id="catalog">
+
+          </section>
+        </div>
+      </section>
     </main>
 </template>
+
+<style scoped lang="less">
+@import "./Styles/storeStyle.less";
+</style>
